7174a9526affc65c544ac9cd3c86c51e8edcf2ed
Fatima <fatima.alyousef1214@gmail.com>
2026-04-19 14:01:47 +0300

    hugging face

--- ingredients_classifier_api.py
+++ ingredients_classifier_api.py
@@ -2,44 +2,53 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from huggingface_hub import hf_hub_download
 import torch
 import json
 
-MODEL_DIR = "model/ingredients-distilbert-classifier"
-LABEL_MAPPING_PATH = "model/labels_mapping.json"
+MODEL_DIR = "Fatima1412/ingredients-distilbert-classifier"
 
-app = FastAPI(title="Ingredients Classifier API")
-
-# Global variables (empty until startup)
 tokenizer = None
 model = None
 id2label = None
+
+app = FastAPI(title="Ingredients Classifier API")
 
 @app.on_event("startup")
 def load_model():
     global tokenizer, model, id2label
 
     try:
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR, local_files_only=True)
-        model = AutoModelForSequenceClassification.from_pretrained(MODEL_DIR, local_files_only=True)
+        # Load tokenizer + model from Hugging Face
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
+        model = AutoModelForSequenceClassification.from_pretrained(MODEL_DIR)
         model.eval()
 
-        with open(LABEL_MAPPING_PATH, "r") as f:
+        # Download labels_mapping.json from Hugging Face
+        mapping_path = hf_hub_download(
+            repo_id=MODEL_DIR,
+            filename="labels_mapping.json"
+        )
+
+        with open(mapping_path, "r") as f:
             id2label = {int(k): v for k, v in json.load(f).items()}
 
-        print("Model loaded successfully.")
+        print("Model and label mapping loaded successfully.")
 
     except Exception as e:
-        print("⚠ WARNING: Model not loaded. Upload model files via Render Shell.")
+        print("⚠ WARNING: Model not loaded. Check Hugging Face repo.")
         print(e)
+
 
 class IngredientsRequest(BaseModel):
     ingredients: str
+
 
 class PredictionResponse(BaseModel):
     label_id: int
     label_name: str
     probabilities: List[float]
+
 
 def predict_ingredients(text):
     inputs = tokenizer(text, return_tensors="pt")
@@ -57,6 +66,7 @@ def predict_ingredients(text):
     label_name = id2label[label_id]
 
     return label_id, label_name, probs
+
 
 @app.post("/predict", response_model=PredictionResponse)
 def predict(req: IngredientsRequest):
